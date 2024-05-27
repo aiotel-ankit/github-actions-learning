ab25d98d895f658d1bb3eb4017acccf47a955faf
iamjangidankit <ankit.jangid@aiotel.com>
2024-05-27 18:48:28 +0530

    added missed files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: "export",
+    basePath: "/github-actions-learning",
+    images: {
+        unoptimized: true,
+      },
+};
 
 export default nextConfig;
